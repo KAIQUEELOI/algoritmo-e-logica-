c22ae789ccd9734b65122d36231e0155e4b4bb47
KAIQUEELOI <kcostakaique21@gmail.com>
2025-07-14 20:54:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//soma
+const   n1 = 25;
+const n2 = 17;
+const soma = n1 + n2;
+console.log(soma);
+// subtraçao
+const n3 = 100;
+const n4 = 250;
+const sub = n4 - n3;
+console.log(sub);
+//mult
+const n5 = 9;
+const n6 = 6;
+const mult = 9 * 6;
+console.log(mult);
+//divisao
+const di1 = 144;
+const di2 = 12;
+const valo = di1/di2;
+console.log(valo);
+//resto
+const r1 = 55;
+const r2 = 6;
+const r3 = 56 % 6;
+console.log(r3);
+//vari
+let x = 10;
+x++;
+console.log(x);
+//vari -
+const y = 5;
+5--;
+console.log("y");
+//desafio 8 
+const 
